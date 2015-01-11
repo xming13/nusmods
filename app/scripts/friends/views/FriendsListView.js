756adc7355bdcf290de0eb5abe8e267d55c78010
Liu Xinan <xinan@me.com>
2015-01-12 04:13:06 +0800

    Removed EmptyView for friend list

--- app/scripts/friends/views/FriendsListView.js
+++ app/scripts/friends/views/FriendsListView.js
@@ -3,14 +3,8 @@
 var _ = require('underscore');
 var Marionette = require('backbone.marionette');
 var FriendsListItemView = require('./FriendsListItemView');
-var template = require('../templates/friends_list_empty.hbs');
-
-var EmptyView = Marionette.ItemView.extend({
-  template: template
-});
 
 module.exports = Marionette.CollectionView.extend({
   tagName: 'tbody',
-  childView: FriendsListItemView,
-  emptyView: EmptyView
+  childView: FriendsListItemView
 });
